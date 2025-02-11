43d430b87b908177ecae75870a665a01d4135c3c
dongnguyenduybang <dndb20011512@gmail.com>
2025-02-11 15:13:47 +0700

    refactor: move function defined response from handles/response.ts to response.ts

--- src/auto_gen/helps/structures/responses.ts
+++ src/auto_gen/helps/structures/responses.ts
@@ -1,22 +1,62 @@
-import { CreateUserDTO } from "src/auto_gen/dtos/create-user.dto";
 import { UserRole } from "src/auto_gen/dtos/enums/user-role.enum";
 
-export interface UserDataResponse {
-    // userId: string,
-    // token: string,
-    username: string,
-    birthday: Date,
-    age: number,
-    isActive: boolean,
-    isObject: object,
-    tags: string[],
-    role: UserRole
+export function transformDataResponse(dtoInstance: any): DataResponse {
+    const instance = Array.isArray(dtoInstance) ? dtoInstance[0] : dtoInstance;
+      return {
+          username: instance.username,
+          birthday: instance.birthday,
+          age: instance.age,
+          isActive: instance.isActive,
+          isObject: instance.isObject,
+          tags: instance.tags,
+          role: instance.role,
+      };
+  }
+  
+  export function successResponse(data: DataResponse[]): SuccessResponse {
+    return {
+        ok: true,
+        data: data,
+    };
+  }
+  
+  export function failResponse(code: number, message: string, details: string[]): FailResponse {
+    return {
+        ok: false,
+        data: null,
+        error: {
+            code,
+            message,
+            details,
+        },
+    };
+  }
+
+export interface DataResponse {
+    username: string;
+    birthday: Date;
+    age: number;
+    isActive: boolean;
+    isObject: object;
+    tags: string[];
+    role: UserRole;
 }
+
+
+export const typeMap: { [key: string]: any } = {
+    username: 'string',
+    birthday: 'Date',
+    age: 'number',
+    isActive: 'boolean',
+    isObject: 'object',
+    tags: 'array', 
+    role: UserRole,
+};
 
 export interface SuccessResponse {
     ok: true;
-    data: UserDataResponse[];
-  }
+    data: DataResponse[]; 
+}
 
 export interface FailResponse {
     ok: false;
