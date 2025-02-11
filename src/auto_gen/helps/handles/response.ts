ba8bd928a8a46d7a0e4a3a83b9a92534b2516686
dongnguyenduybang <dndb20011512@gmail.com>
2025-02-11 15:16:53 +0700

    refactor: delete function defined response

--- src/auto_gen/helps/handles/response.ts
+++ src/auto_gen/helps/handles/response.ts
@@ -1,51 +1,14 @@
 
-import { SuccessResponse, UserDataResponse, FailResponse } from "../structures/responses";
+import { SuccessResponse, FailResponse, transformDataResponse, successResponse, failResponse } from "../structures/responses";
 
-const crypto = require('crypto');
-
-export function transformToUserDataResponse(dtoInstance: any): UserDataResponse {
-  const instance = Array.isArray(dtoInstance) ? dtoInstance[0] : dtoInstance;
-    return {
-        // userId: crypto.randomBytes(13).toString('hex'),
-        // token: crypto.randomBytes(20).toString('hex'),
-        username: instance.username,
-        birthday: instance.birthday,
-        age: instance.age,
-        isActive: instance.isActive,
-        isObject: instance.isObject,
-        tags: instance.tags,
-        role: instance.role,
-    };
-}
-
-export function createSuccessResponse(data: UserDataResponse[]): SuccessResponse {
-  return {
-      ok: true,
-      data: data,
-  };
-}
-
-export function createFailResponse(code: number, message: string, details: string[]): FailResponse {
-  return {
-      ok: false,
-      data: null,
-      error: {
-          code,
-          message,
-          details,
-      },
-  };
-}
-
-export function handleResponse(data: { ok: boolean; data: any; errors?: { [key: string]: any } }) {
-
-  if (data.ok) {
-
-      const userData = transformToUserDataResponse(data.data);
-      return createSuccessResponse([userData]);
+export function handleResponse(data: { ok: boolean; data: any; errors?: { [key: string]: any } }): SuccessResponse | FailResponse {
+  if (data.ok === true ) {
+      const dataResponse = transformDataResponse(data.data);
+      return successResponse([dataResponse]);
   } else {
-
-      const errorDetails = Object.values(data.errors || {}).flat(); 
-      return createFailResponse(1000, "Invalid argument", errorDetails); 
+      const errorDetails = Object.values(data.errors || {}).flat();
+      return failResponse(1000, "Invalid argument", errorDetails);
   }
 }
+
+
