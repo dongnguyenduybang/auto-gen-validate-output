bb2072dff7978366cf815d0e1d4f325f36a16c80
dongnguyenduybang <dndb20011512@gmail.com>
2025-02-14 13:51:45 +0700

    feat: add response dto

--- src/auto_gen/dtos/mock-user.dto.ts
+++ src/auto_gen/dtos/mock-user.dto.ts
@@ -2,25 +2,23 @@
 import 'reflect-metadata';
 import { IsAny, IsEnum, IsObject, IsArray, IsBoolean, IsDate, IsNotEmpty, IsNotNull, IsNumber, IsOptional, IsString, MaxArray, MaxLength, Min, MinArray, MinLength, Max } from '../decorator/dto-decorator';
 
-
-
 export class MockUserDTO {
+  
 
-    @IsString()
-    @IsNotEmpty()
-    @IsNotNull()
-    prefix?: string = 'duy12345'
+  @IsString()
+  @IsNotEmpty()
+  @IsNotNull()
+  prefix?: string = 'duy12345';
 
 
-    @IsNumber()
-    @IsNotEmpty()
-    @IsNotNull()
-    quantity?: number = 1;
+  @IsNumber()
+  @IsNotEmpty()
+  @IsNotNull()
+  quantity?: number = 1;
 
-    @IsOptional()
-    @IsNumber()
-    @IsNotEmpty()
-    @IsNotNull()
-    badge?: number = 0;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsNotNull()
+  badge?: number = 0;
 }
-
