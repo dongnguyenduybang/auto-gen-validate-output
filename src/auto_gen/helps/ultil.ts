8d7b4b316bbdfc03a84ef83815b2a2aa5b161859
dongnguyenduybang <dndb20011512@gmail.com>
2025-02-14 13:50:35 +0700

    feat: add ultil get pairFile

--- src/auto_gen/helps/ultil.ts
+++ src/auto_gen/helps/ultil.ts
@@ -1,6 +1,9 @@
 import * as path from 'path';
 import * as fs from 'fs';
+import 'reflect-metadata';
 
+import { getMetadataStorage } from 'class-validator';
+import { ValidationRule } from './structures/responses';
 
 
 function getFileNameWithoutExtension(filePath: string): string {
@@ -11,22 +14,22 @@ function getFileNameWithoutExtension(filePath: string): string {
 export function pairFiles(
     requestsDir: string,
     payloadsDir: string
-  ): { apiConfig: string; payload: string; name: string}[] {
+  ): { requestConfig: string; payload: string; name: string}[] {
     const requestFiles = fs.readdirSync(requestsDir);
     const payloadFiles = fs.readdirSync(payloadsDir);
   
-    const pairedFiles: { apiConfig: string; payload: string; name: string }[] = [];
+    const pairedFiles: { requestConfig: string; payload: string; name: string }[] = [];
   
     for (const requestFile of requestFiles) {
-      const apiFileName = getFileNameWithoutExtension(requestFile);
+      const requestFileName = getFileNameWithoutExtension(requestFile);
   
       for (const payloadFile of payloadFiles) {
         const payloadFileName = getFileNameWithoutExtension(payloadFile);
   
-        if (apiFileName === payloadFileName) {
+        if (requestFileName === payloadFileName) {
           pairedFiles.push({
-            name: apiFileName,
-            apiConfig: path.join(requestsDir, requestFile),
+            name: requestFileName,
+            requestConfig: path.join(requestsDir, requestFile),
             payload: path.join(payloadsDir, payloadFile),
           });
         }
@@ -34,4 +37,91 @@ export function pairFiles(
     }
   
     return pairedFiles;
+  }
+
+
+  export function getValidationFromDTOResponse(dtoClass: any): ValidationRule[] {
+    const rulesPush: Record<string, Partial<ValidationRule>> = {};
+    const metadataStorage = getMetadataStorage();
+    const validationMetadatas = metadataStorage.getTargetValidationMetadatas(
+      dtoClass,
+      '',
+      false,
+      false
+    );
+  
+    for (const metadata of validationMetadatas) {
+      const field = metadata.propertyName;
+  
+      if (!rulesPush[field]) {
+        rulesPush[field] = { field };
+      }
+  
+      const rule = rulesPush[field];
+  
+      if (metadata.name === 'isString') {
+        rule.type = 'string';
+      } else if (metadata.name === 'isNumber') {
+        rule.type = 'number';
+      } else if (metadata.name === 'isBoolean') {
+        rule.type = 'boolean';
+      } else if (metadata.name === 'isArray') {
+        rule.type = 'array';
+      } else if (metadata.name === 'isObject') {
+        rule.type = 'object';
+      }
+  
+      if (metadata.type === 'isDefined' || metadata.type === 'isNotEmpty') {
+        rule.required = true;
+      }
+  
+      if (metadata.constraints && metadata.constraints.length > 0) {
+        
+        metadata.constraints.forEach((constraint: any) => {
+          if (typeof constraint === 'number') {
+            if (metadata.name === 'min') {
+              rule.min = constraint;
+            } else if (metadata.name === 'max') {
+              rule.max = constraint;
+            }
+          } else if (typeof constraint === 'string') {
+            
+          }
+        });
+      }
+  
+      if (metadata.type === 'customValidation') {
+
+        if (metadata.name === 'minLength') {
+          rule.minLength = metadata.constraints[0];
+        } else if (metadata.name === 'maxLength') {
+
+          rule.maxLength = metadata.constraints[0];
+        } else if (metadata.name === 'arrayMinSize') {
+
+          rule.minArray = metadata.constraints[0];
+        } else if (metadata.name === 'arrayMaxSize') {
+
+          rule.maxArray = metadata.constraints[0];
+        } else if (metadata.name === 'minDate') {
+
+          rule.minDate = typeof metadata.constraints[0] === 'function'
+            ? metadata.constraints[0]()
+            : metadata.constraints[0];
+        } else if (metadata.name === 'maxDate') {
+          
+          rule.maxDate = typeof metadata.constraints[0] === 'function'
+            ? metadata.constraints[0]()
+            : metadata.constraints[0];
+        }
+      }
+    }
+    Object.keys(rulesPush).forEach(field => {
+      const rule = rulesPush[field];
+      if (rule.required === undefined) {
+          rule.optional = true; 
+      }
+  });
+  
+    return Object.values(rulesPush) as ValidationRule[];
   }
