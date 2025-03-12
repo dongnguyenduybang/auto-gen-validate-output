470deb7c3fb4fd005c207e043c87df8307ccc955
dongnguyenduybang <dndb20011512@gmail.com>
2025-03-12 15:13:14 +0700

    example

---
+++ src/auto-gen/response/mock-user.response.ts
@@ -0,0 +1,45 @@
+
+import { Type } from 'class-transformer';
+import { IsBoolean, IsArray, IsString, StartWith, EndWith, IsEnum, IsDefined } from '../decorator/dto-decorator';
+
+import { BadgeEnum } from '../enums/badge.enum';
+export class UserData {
+
+    @IsString()
+    @IsDefined()
+    userId: string;
+
+    @IsString()
+    @IsDefined()
+    @StartWith('prefix')
+    @EndWith('userId')
+    username: string;
+
+    @IsString()
+    @IsDefined()
+    token: string;
+
+    @IsString()
+    @IsDefined()
+    securityKey: string;
+
+    @IsString()
+    @IsDefined()
+    recoverKey: string;
+
+    @IsEnum(BadgeEnum)
+    @IsDefined()
+    badge: BadgeEnum;
+}
+
+
+export class MockUserResponse {
+    @IsBoolean()
+    @IsDefined()
+    ok: boolean;
+
+    @IsArray()
+    @IsDefined()
+    @Type(() => UserData)
+    data: UserData[];
+}
