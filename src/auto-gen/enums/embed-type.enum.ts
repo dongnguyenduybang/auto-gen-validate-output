2adeda742a21d09822361b3c83070ee330bdf50a
dongnguyenduybang <dndb20011512@gmail.com>
2025-03-12 15:04:25 +0700

    fix all

---
+++ src/auto-gen/enums/embed-type.enum.ts
@@ -0,0 +1,9 @@
+export enum EmbedTypeEnum {
+    EMBED_TYPE_ENUM_UNSPECIFIED = 0,//	The embed is unspecified
+    EMBED_TYPE_ENUM_PHOTO = 1,//The embed is a photo type
+    EMBED_TYPE_ENUM_VIDEO = 2,//	The embed is video type
+    EMBED_TYPE_ENUM_LINK = 3,//	The embed is link type
+    EMBED_TYPE_ENUM_INVITATION = 4,//The embed is invitation
+    EMBED_TYPE_ENUM_OTHER = 5,//	The embed has other type
+    EMBED_TYPE_ENUM_LOCATION = 6//	The embed is location
+}
