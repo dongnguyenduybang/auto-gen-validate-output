b7543f66ee2c62c799f2957ed0ef9d6666966bb5
dongnguyenduybang <dndb20011512@gmail.com>
2025-03-20 09:38:07 +0700

    fix: fix jest setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,16 +1,17 @@
+/* eslint-disable prettier/prettier */
 import { initializeGlobalVariables } from './src/auto-gen/global-var';
-import { setupConfiguration, getOrThrow } from './src/auto-gen/helps/get-config';
+import {
+  setupConfiguration,
+  getOrThrow,
+} from './src/auto-gen/helps/get-config';
 setupConfiguration();
 
-// initializeGlobalVariables();
+initializeGlobalVariables();
 
 try {
-
   globalThis.url = getOrThrow<string>('host');
   globalThis.globalVariables = new Map<string, any>();
-  globalThis.local = 'http://localhost:3000'
+  globalThis.local = 'http://localhost:3000';
 } catch (error) {
-
   console.error('Error:', error.message);
-  
 }
