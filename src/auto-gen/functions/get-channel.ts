28978b12bfa71a4a3d82a2171e76d52837073b20
dongnguyenduybang <dndb20011512@gmail.com>
2025-03-20 09:41:12 +0700

    feat: add func get message/channel

---
+++ src/auto-gen/functions/get-channel.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import axios from 'axios';
+export async function getChannel(header,body) {
+    try {
+        if (!header.token) {
+            return { error: 'Token not found to get channel' };
+        }
+        const baseUrl = `${globalThis.urls}/ChannelView/GetChannel?workspaceId=${body.workspaceId}&channelId=${body.channelId}`;
+        const headers = { 'x-session-token': header.token };
+        const response = await axios.get(baseUrl, { headers: headers });
+        if (!response.data || !response.data.data || !response.data.data.channel) {
+            return { error: 'Invalid data get channel returned from API' };
+        } else {
+            return { ok: true, result: response.data };
+        }
+    } catch (error) {
+        return { ok: false, result: error.response.data.error.details };
+    }
+}
--- src/auto-gen/functions/get-message.ts
+++ src/auto-gen/functions/get-message.ts
@@ -1,32 +1,24 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import axios from 'axios';
-import {
-  resolveJsonVariables,
-  resolveVariables,
-} from '../helps/get-resolve-variables';
-export async function getMessage(
-  tokenParam: string,
-  channelIdParam: string,
-  messageIdParam: any,
-) {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function getMessage(header, body) {
   try {
-    const token = resolveVariables(tokenParam);
-    const channelId = resolveVariables(channelIdParam);
-    const messageId = resolveVariables(messageIdParam);
-    const baseUrl = `${globalThis.urls}/MessageView/GetMessage?workspaceId=0&channelId=${channelId}&messageId=${messageId}`;
-    const header = { 'x-session-token': token };
-
-    const response = await axios.get(baseUrl, { headers: header });
-    if (response.data) {
-      const content = response?.data?.data?.message?.content || [];
-      const ref = response?.data?.data?.message?.ref || [];
-      globalThis.globalVar.set('content', content);
-      globalThis.globalVar.set('ref', ref);
-      return response.data;
-    } else {
-      throw new Error('No data in response');
-    }
+    // if (!header.token) {
+    //   return { error: 'Token not found to create channel' };
+    // }
+    // const baseUrl = `${globalThis.urls}/MessageView/GetMessage?workspaceId=0&channelId=${channelId}&messageId=${messageId}`;
+    // const header = { 'x-session-token': header.token };
+    // const response = await axios.get(baseUrl, { headers: header });
+    // if (!response.data || !response.data.data || !response.data.data.message) {
+    //   return { error: 'Invalid data get message returned from API' };
+    // } else {
+    //   const content = response?.data?.data?.message?.content || [];
+    //   const ref = response?.data?.data?.message?.ref || [];
+    //   globalThis.globalVar.set('content', content);
+    //   globalThis.globalVar.set('ref', ref);
+    //   return { ok: true, result: response.data };
+    // }
   } catch (error) {
-    console.error('Error in get message:', error);
-    throw new Error('Fail to get message');
+    return { ok: false, result: error.response.data.error.details };
   }
 }
