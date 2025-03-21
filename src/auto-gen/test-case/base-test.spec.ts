8d220192c783f70ae6e5e071bfdb9f0cca434705
dongnguyenduybang <dndb20011512@gmail.com>
2025-03-21 14:27:56 +0700

    feat: base test

--- src/auto-gen/test-case/base-test.spec.ts
+++ src/auto-gen/test-case/base-test.spec.ts
@@ -1,132 +1,119 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
 /* eslint-disable prefer-const */
-import fs from 'fs';
-import path from 'path';
-import { summarizeErrors, getTime } from '../helps/utils';
 import { executeBeforeAllSteps } from '../functions';
+import { TestContext } from '../text-context';
+import path from 'path';
+import fs from 'fs';
+import { plainToClass } from 'class-transformer';
+import { SendMessageResponse } from '../response/send-message.response';
+import { validateResponses } from '../validates/validate-response';
+import axios from 'axios';
+import { resolveObject } from '../helps/get-resolve-variables';
+let globalContext, headerRequest;
+let failedStep = [];
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+let nextStep;
 
-describe('Testcase for send-message', () => {
-  let totalTests = 0;
-  let passedLogic = 0;
-  let failedTests = [];
-  let logicTests = [];
-  let passedTests = 0;
-  let passed200 = 0;
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  let headerRequest;
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  let testNumber;
-  let resolvedData;
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  let nextStep = false;
-  let failedStep = [];
+beforeAll(async () => {
+  globalContext = new TestContext();
+  const beforeStep = await executeBeforeAllSteps(
+    [
+      "mockUser(body: { prefix: 'duy12345',quantity: 1,badge: 0 }, expect: {})",
+      "createChannel(body: {name: 'channel 1', workspaceId: '0'}, header: {token: '{{mockUser.token}}'}, expect: {isOwner: '{{mockUser.userId}}' } )",
+      "getChannel(body: {workspaceId: '0', channelId: '{{createChannel.isChannelId}}'}, header: {token: '{{mockUser.token}}'}, expect: {countMember: 1, countMessage: 1, isChannelId: '{{createChannel.isChannelId}}',isLastMessage: '{{createChannel.messageId}}', isContent: '{{createChannel.isContent}}', isOwner: '{{mockUser.userId}}'})"
+    ],
+    globalContext
+  );
+  failedStep.push({
+    error: beforeStep,
+  });
+  headerRequest = {
+    'Content-Type': 'application/json',
+    'x-session-token': '{{mockUser.token}}',
+    'x-country-code': 'VN',
+  };
+});
 
-  beforeAll(async () => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const beforeStep = await executeBeforeAllSteps([
-        "mockUser(body: { prefix: 'duy12345',quantity: 1,badge: 0 }, expect: {})",
-        "createChannel(body: {name: 'channel 1', workspaceId: '0'}, header: {token: '{{mockUser.token}}'}, expect: {isContent: '%s created this channel', isOwner: '{{mockUser.userId}}'})",
-        "getChannel(body: {workspaceId: '0', channelId: '{{createChannel.channelId}}'}, header: {token: '{{mockUser.token}}'}, expect: {countMember: 1, countMessage: 1, isChannelId: '{{createChannel.channelId}}',isLastMessage: '{{createChannel.messageId}}', isContent: '{{createChannel.contentChannel}}', isOwner: '{{mockUser.userId}}'})",
-      ]);
-    failedStep.push({
-      error: beforeStep
-    });
-    headerRequest = {
-      'Content-Type': 'application/json',
-      'x-session-token': '{{token}}',
-      'x-country-code': 'VN',
+it('Test case # 43 with expect errors []', async () => {
+  const payloadObj: any = {
+    workspaceId: '0',
+    channelId: '{{getChannel.isChannelId}}',
+    content: 'test123123',
+  };
+
+  const resolvedPayload = resolveObject(payloadObj, globalContext);
+  const resolvedHeader = resolveObject(headerRequest, globalContext);
+  console.log(resolvedHeader);
+  const response = await axios.post(
+    `${globalThis.url}/Message/SendMessage`,
+    resolvedPayload,
+    {
+      headers: {
+        ...resolvedHeader
+      },
+    },
+  );
+  const validatedResponse = plainToClass(SendMessageResponse, response.data);
+  const result = validateResponses(validatedResponse, SendMessageResponse);
+  if (result.length !== 0) {
+    
+  } else {
+    const response = validatedResponse.data.message
+    const sendMessageContext = {
+      messageId: response.messageId,
+      content: response.content,
+      channelId: response.channelId,
+      senderId: response.userId,
+      expect: {},
     };
-  });
+    globalContext.setStepContext('sendMessage', sendMessageContext);
+    nextStep = true;
+  };
+});
+afterAll(async () => {
 
-  it('test asas', async () => {
-    expect(10).toEqual(10);
-  });
-  afterAll(async () => {
-    const folderPath = path.join(__dirname, '../reports');
-
-    const folderPathLogic = path.join(__dirname, '../reports/send-message');
-    if (!fs.existsSync(folderPath)) {
-      fs.mkdirSync(folderPath, { recursive: true });
-    }
-    if (!fs.existsSync(folderPathLogic)) {
-      fs.mkdirSync(folderPathLogic, { recursive: true });
-    }
-    const summary = summarizeErrors(
-      failedTests,
-      totalTests,
-      passedLogic,
-      passed200,
+  if(nextStep === true){
+    const afterAllStep = await executeBeforeAllSteps(
+      [
+         "getListMessage(body: { workspaceId: '0', channelId: '{{sendMessage.channelId}}' }, header: {token: '{{mockUser.token}}' }, expect: {isLastMessage: '{{sendMessage.messageId}}', isLastContent: '{{sendMessage.content}}', isSender: '{{sendMessage.senderId}}' })"
+      ],
+      globalContext
     );
-    const resultContent = `
+    failedStep.push({
+      error: afterAllStep,
+    });
+  }
+
+  const folderPath = path.join(__dirname, '../reports');
+
+  const folderPathLogic = path.join(__dirname, '../reports/send-message');
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath, { recursive: true });
+  }
+  if (!fs.existsSync(folderPathLogic)) {
+    fs.mkdirSync(folderPathLogic, { recursive: true });
+  }
+  const resultContent = `
 === Test Reports for DTO "send-message" ===
 Host: ${globalThis.url}
 Endpoint: /Message/SendMessage
-Summary: \n
-Total Tests: ${totalTests}
-Passed Tests: ${passedTests}
-Failed Tests: ${failedTests.length}
-
 Step Log: ${failedStep
-  .map(failStep => {
-    const errorMessage = failStep.error ? `Error: ${JSON.stringify(failStep.error, null, 2)}` : 'No Error';
-    return `${errorMessage}`;
-  })
-  .join('')}
+    .map((failStep) => {
+      const errorMessage = failStep.error
+        ? `Error: ${JSON.stringify(failStep.error, null, 2)}`
+        : 'No Error';
+      return `${errorMessage}`;
+    })
+    .join('')}
+    `;
 
-
-Status Code:
-  200: ${summary.statusCodes[200] || 0}
-  201: ${summary.statusCodes[201] || 0}
-  400: ${summary.statusCodes[400] || 0}
-  403: ${summary.statusCodes[403] || 0}
-  404: ${summary.statusCodes[404] || 0}
-  500: ${summary.statusCodes[500] || 0}
-Uniques Error:
-  ${Array.from(summary.uniqueErrors.entries())
-        .map(
-          ([error, count]) => `${error}: ${count} 
- `,
-        )
-        .join('')}
-Failed Test Details:
-${failedTests
-        .map(
-          (failCase) => `
-  - Testcase #${failCase.testcase}
-    Missing Errors: ${failCase.missing ? JSON.stringify(failCase.missing) : "''"}
-    Status Code: ${failCase.code ? JSON.stringify(failCase.code) : "''"}
-    Extra Errors: ${failCase.extra ? JSON.stringify(failCase.extra) : "''"}
-    Detail Errors: ${failCase.errorDetails ? JSON.stringify(failCase.errorDetails) : "''"}`,
-        )
-        .join('')}`;
-
-    const resultLogicError = `
-    === Test Reports Logic for DTO "send-message" ===
-    Host: ${globalThis.url}
-    Endpoint: /Message/SendMessage
-    Error: 
-    ${logicTests
-        .map(
-          (logicCaseFail) => `
-    - Testcase #${logicCaseFail.testcase}
-      Logic Errors: ${logicCaseFail.errorLogic ? JSON.stringify(logicCaseFail.errorLogic) : "''"}
-  `,
-        )
-        .join('')}`;
-
-    const resultFilePath = path.join(folderPath, 'send-message.txt');
-    const resultFilePathLogic = path.join(
-      folderPathLogic,
-      `send-message.${getTime()}.txt`,
-    );
-    fs.writeFileSync(resultFilePath, resultContent, 'utf-8');
-    fs.writeFileSync(resultFilePathLogic, resultLogicError, 'utf-8');
-    console.log(`Success: ${resultFilePath}`);
-    // await executeDelete(
-    //   ["deleteMessageForEveryone('0', {{channelId}}, {{messageId}})"],
-    //   headerRequest,
-    // );
-  });
+  const resultFilePath = path.join(folderPath, 'send-message.txt');
+  fs.writeFileSync(resultFilePath, resultContent, 'utf-8');
+  console.log(`Success: ${resultFilePath}`);
+  // await executeDelete(
+  //   ["deleteMessageForEveryone('0', {{channelId}}, {{messageId}})"],
+  //   headerRequest,
+  // );
 });
