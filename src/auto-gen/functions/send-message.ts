b5a52139d887c216e4ea53c5a65157b289dfa8df
dongnguyenduybang <dndb20011512@gmail.com>
2025-04-08 17:17:11 +0700

    fix: fix axios add method, path

--- src/auto-gen/functions/send-message.ts
+++ src/auto-gen/functions/send-message.ts
@@ -1,18 +1,19 @@
 import axios from 'axios';
 
-export async function sendMessage(header, body) {
+export async function sendMessage(method, path, header, body) {
   try {
     if (!header.token) {
       return { error: 'Token not found to send message' };
     }
-    const baseUrl = `${globalThis.urls}/Message/SendMessage`;
+    const baseUrl = `${globalThis.urls}${path}`;
+    const methodLowCase = method.toLowerCase();
     const payload = {
       workspaceId: body.workspaceId,
       channelId: body.channelId,
       content: body.content,
     };
     const headers = { 'x-session-token': header.token };
-    const response = await axios.post(baseUrl, payload, { headers: headers });
+    const response = await axios[methodLowCase](baseUrl, payload, { headers: headers });
     if (!response.data || !response.data.data || !response.data.data.message) {
       return {
         ok: false,
@@ -24,10 +25,14 @@ export async function sendMessage(header, body) {
   } catch (error) {
     return {
       ok: false,
-      response:
-        error?.response?.data?.error?.details ||
+      response: {
+        status: error.response.status,
+        statusText: error.response.statusText,
+        detailError: error?.response?.data?.error?.details ||
         error?.response?.data ||
         'Unauthorized request',
+      }
+       
     };
   }
 }
