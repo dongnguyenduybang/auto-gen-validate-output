29729b13ad58f8e05ee9e2511969c6db3110ff19
dongnguyenduybang <dndb20011512@gmail.com>
2025-04-08 17:17:36 +0700

    fix: add method, path

--- src/auto-gen/test-execute-step/api-function.ts
+++ src/auto-gen/test-execute-step/api-function.ts
@@ -3,16 +3,18 @@ import { acceptInvitation } from '../functions/accept-link-invitation';
 import { createChannel } from '../functions/create-channel';
 import { getChannel } from '../functions/get-channel';
 import { mockUser } from '../functions/mock-user';
+import { sendMessage } from '../functions/send-message';
 import { TestContext } from './text-context';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export function getApiFunction(action: string, context: TestContext): Function {
   // eslint-disable-next-line @typescript-eslint/ban-types
   const apiFunctions: Record<string, Function> = {
-    mockUser: (header, body) => mockUser(header, body),
+    mockUser: (method, path, header, body) => mockUser(header, body),
     createChannel: () => createChannel(context),
-    getChannel: (header, body) => getChannel(header, body),
-    acceptInvitation:(header, body) => acceptInvitation(header, body)
+    getChannel: (method, path, header, body) => getChannel(header, body),
+    acceptInvitation:(method, path, header, body) => acceptInvitation(header, body),
+    sendMessage:(method, path, header, body) => sendMessage(method, path, header, body)
   };
 
   return (
