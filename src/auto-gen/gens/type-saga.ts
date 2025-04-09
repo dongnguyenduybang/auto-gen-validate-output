4f47d4e2bfaeb88489fd2353204b910c171940c4
dongnguyenduybang <dndb20011512@gmail.com>
2025-04-10 01:05:42 +0700

    delete: delete unused

--- src/auto-gen/gens/type-saga.ts
+++
@@ -1,8 +0,0 @@
-// import { SendMessageSaga } from "../dtos/send-message/send-message.request";
-
-// export const Sagas = {
-//     sendMessage: SendMessageSaga,
-//     // thêm các saga khác ở đây
-//   };
-  
-//   export type SagaType = keyof typeof Sagas;
