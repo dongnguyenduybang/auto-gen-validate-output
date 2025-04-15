ee53d8da8ab7e4c06c8192c3f7ccb35914a65acb
dongnguyenduybang <dndb20011512@gmail.com>
2025-04-15 15:28:05 +0700

    feat: invalid ulid enum

--- src/auto-gen/enums/error-message.enum.ts
+++ src/auto-gen/enums/error-message.enum.ts
@@ -23,4 +23,5 @@ export enum ErrorMessage {
   START_WITH = 'must start with',
   END_WITH = 'must end with',
   INVALID_RANGE_STRING_LENGTH = "content's byte length must fall into (1, 6000) range",
+  INVALID_ULID = 'must be an ULID'
 }
