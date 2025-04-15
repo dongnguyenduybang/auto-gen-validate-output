b5a64ca9a0a29974804e430a3ea24fa5037339c6
dongnguyenduybang <dndb20011512@gmail.com>
2025-04-15 16:55:32 +0700

    feat: add update message saga

---
+++ src/auto-gen/test-sagas/update-message/update-message.saga.ts
@@ -0,0 +1,177 @@
+import { Operator } from '../../enums/operator.enum';
+import { Element } from '../../enums/element.enum';
+
+export const UpdateMessageSaga = {
+  steps: [
+    {
+      action: 'mockUser',
+    },
+    {
+      action: 'createChannel',
+    },
+    {
+      action: 'sendMessage',
+      method: 'POST',
+      path: '/Message/SendMessage',
+      body: {
+        workspaceId: '0',
+        channelId: '{{channelId}}',
+        content: 'user send message',
+        ref: 'ref',
+      },
+      header: { token: '{{token}}' },
+      expect: {
+        ok: { operator: Operator.EQUAL, expect: true },
+      }
+    },
+    {
+      action: 'updateMessage',
+      method: 'PUT',
+      path: '/Message/UpdateMessage',
+      body: {
+        workspaceId: '0',
+        channelId: '{{channelId}}',
+        messageId: '{{messageId1}}',
+        content: 'user message update'
+      },
+      header: { token: '{{token}}' },
+      expect: {
+        ok: { operator: Operator.EQUAL, expect: true },
+        data: {
+          message: {
+            workspaceId: { operator: Operator.EQUAL, expect: 0 },
+            channelId: { operator: Operator.EQUAL, expect: '{{channelId}}' },
+            userId: { operator: Operator.EQUAL, expect: '{{userId}}' },
+            content: { operator: Operator.EQUAL, expect: 'user message update' },
+            messageType: { operator: Operator.EQUAL, expect: 0 },
+            messageStatus: { operator: Operator.EQUAL, expect: 1 },
+            attachmentType: { operator: Operator.EQUAL, expect: 0 },
+          },
+        },
+        includes: {
+          users: [
+            {
+              field: 'userId',
+              operator: Operator.INCLUDE,
+              element: Element.FIRST,
+              expect: ['{{userId}}'],
+            },
+            {
+              field: 'userType',
+              operator: Operator.INCLUDE,
+              element: Element.FIRST,
+              expect: 0,
+            },
+            {
+              field: 'profile.userBadgeType',
+              operator: Operator.EQUAL,
+              expect: 0,
+            },
+          ],
+          channels: [
+            {
+              field: 'workspaceId',
+              operator: Operator.EQUAL,
+              element: Element.FIRST,
+              expect: 0,
+            },
+            {
+              field: 'channelId',
+              operator: Operator.EQUAL,
+              element: Element.FIRST,
+              expect: ['{{channelId}}'],
+            },
+            {
+              field: 'userId',
+              operator: Operator.EQUAL,
+              element: Element.FIRST,
+              expect: ['{{userId}}'],
+            },
+            {
+              field: 'totalMembers',
+              operator: Operator.EQUAL,
+              element: Element.FIRST,
+              expect: ['{{totalMembers}}'],
+            },
+            {
+              field: 'name',
+              operator: Operator.EQUAL,
+              element: Element.FIRST,
+              expect: ['{{name}}'],
+            },
+          ],
+          members: [
+            {
+              field: 'workspaceId',
+              operator: Operator.INCLUDE,
+              element: Element.ALL,
+              expect: 0,
+            },
+            {
+              field: 'channelId',
+              operator: Operator.INCLUDE,
+              element: Element.ALL,
+              expect: ['{{channelId}}'],
+            },
+            {
+              field: 'userId',
+              operator: Operator.INCLUDE,
+              element: Element.FIRST,
+              expect: ['{{userId}}'],
+            },
+            {
+              field: 'role',
+              operator: Operator.INCLUDE,
+              element: Element.FIRST,
+              expect: ['owner'],
+            },
+            {
+              field: 'roles.role',
+              operator: Operator.INCLUDE,
+              element: Element.ALL,
+              expect: ['everyone', 'owner'],
+            },
+          ],
+          channelMetadata: [
+            {
+              field: 'workspaceId',
+              operator: Operator.INCLUDE,
+              element: Element.ALL,
+              expect: 0,
+            },
+            {
+              field: 'channelId',
+              operator: Operator.INCLUDE,
+              element: Element.ALL,
+              expect: ['{{channelId}}'],
+            },
+          ],
+        },
+      },
+    },
+    {
+      action: 'acceptInvitation',
+      body: { linkInvitation: '{{invitationLink}}' },
+      header: { token: '{{token1}}' },
+      expect: {
+        ok: { operator: Operator.EQUAL, expect: true },
+      },
+    },
+    {
+      action: 'getChannel',
+      body: { channelId: '{{channelId}}' },
+      header: { token: '{{token}}' },
+      expect: {
+        ok: { operator: Operator.EQUAL, expect: true },
+      },
+    },
+    {
+      action: 'deleteMessagesForEveryone',
+      body: { channelId: '{{channelId}}', messageId: '{{messageId1}}' },
+      header: { token: '{{token}}' },
+      expect: {
+        ok: { operator: Operator.EQUAL, expect: true },
+      },
+    },
+  ],
+};
