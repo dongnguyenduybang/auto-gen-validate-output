7cd8b6236ffc03a6831b707c05e44bc2471aaa9e
dongnguyenduybang <dndb20011512@gmail.com>
2025-04-23 17:29:24 +0700

    feat: add resume

--- src/auto-gen/test-ws/send-message/send-message.ws.ts
+++ src/auto-gen/test-ws/send-message/send-message.ws.ts
@@ -54,11 +54,6 @@ export const SendMessageSagaWS = {
     },
   ],
   steps: [
-    // {
-    //   action: 'wsActorResume',
-    //   path: VAR.url,
-    //   body: VAR.idWS
-    // },
     {
       action: 'createChannel',
       method: METHOD.POST,
@@ -89,119 +84,134 @@ export const SendMessageSagaWS = {
         ref: 'ref',
       },
       headers: HeaderList.Token(),
-      expect: {
-        ok: { operator: Operator.EQUAL, expect: true },
+    },
+    {
+      action: 'resume',
+      path: 'wsActor',
+      body: "member_joined.id",
+      
+    },
+    {
+      action: 'sendMessage',
+      method: METHOD.POST,
+      path: APIPath.Message.SendMessage,
+      body: {
+        workspaceId: '0',
+        channelId: VAR.channelId,
+        content: 'user send message 2',
+        ref: 'ref',
       },
+      headers: HeaderList.Token(),
     },
   ],
-  // wssCheck:
-  // {
-  //   expectWSActor: [
-  //     {
-  //       type: { operator: Operator.EQUAL, expect: API_EVENT.halome.v3.chat.CHANNEL_CREATED },
-  //       data: {
-  //         channel: {
-  //           workspaceId: { operator: Operator.EQUAL, expect: '0' },
-  //           channelId: { operator: Operator.EQUAL, expect: VAR.channelId },
-  //           userId: { operator: Operator.EQUAL, expect: VAR.userId },
-  //         }
-  //       },
-  //       includes: {
-  //         users: [
-  //           {
-  //             field: 'userId',
-  //             operator: Operator.INCLUDE,
-  //             element: Element.FIRST,
-  //             expect: [VAR.userId],
-  //           },
-  //           {
-  //             field: 'userType',
-  //             operator: Operator.INCLUDE,
-  //             element: Element.FIRST,
-  //             expect: 0,
-  //           },
-  //           {
-  //             field: 'profile.userBadgeType',
-  //             operator: Operator.EQUAL,
-  //             expect: 0,
-  //           },
-  //         ],
-  //       }
-  //     },
-  //     {
-  //       type: { operator: Operator.EQUAL, expect: API_EVENT.halome.v3.chat.MEMBER_JOINED },
-  //       data: {
-  //         workspaceId: { operator: Operator.EQUAL, expect: '0' },
-  //         channelId: { operator: Operator.EQUAL, expect: VAR.channelId },
-  //         joinedUserId: { operator: Operator.EQUAL, expect: VAR.userId1 }
-  //       }
-  //     },
-  //     {
-  //       type: { operator: Operator.EQUAL, expect: API_EVENT.halome.v3.chat.MESSAGE_CREATED },
-  //       data: {
-  //         message: {
-  //           channelId: { operator: Operator.EQUAL, expect: VAR.channelId },
-  //           content: { operator: Operator.EQUAL, expect: '%s joined this channel' },
-  //           messageType: { operator: Operator.EQUAL, expect: 1 }
-  //         }
-  //       }
-  //     },
-  //     {
-  //       type: { operator: Operator.EQUAL, expect: API_EVENT.halome.v3.chat.MESSAGE_CREATED },
-  //       data: {
-  //         message: {
-  //           workspaceId: { operator: Operator.EQUAL, expect: '0' },
-  //           channelId: { operator: Operator.EQUAL, expect: VAR.channelId },
-  //           content: { operator: Operator.EQUAL, expect: 'user send message' },
-  //           userId: { operator: Operator.EQUAL, expect: VAR.userId },
-  //           messageType: { operator: Operator.EQUAL, expect: 0 }
-  //         }
-  //       }
-  //     },
-  //     {
-  //       type: { operator: Operator.EQUAL, expect: API_EVENT.halome.v3.chat.USER_UNREAD_MESSAGE_UPDATED },
-  //       data: {
-  //         message: {
-  //           lastSeenMessageId: { operator: Operator.EQUAL, expect: VAR.messageId3 },
-  //           channelId: { operator: Operator.EQUAL, expect: VAR.channelId },
-  //         }
-  //       },
-  //     }
-  //   ],
-  //   expectWSRecipient: [
-  //     {
-  //       type: { operator: Operator.EQUAL, expect: API_EVENT.halome.v3.chat.MEMBER_JOINED },
-  //       data: {
-  //         workspaceId: { operator: Operator.EQUAL, expect: '0' },
-  //         channelId: { operator: Operator.EQUAL, expect: VAR.channelId },
-  //         joinedUserId: { operator: Operator.EQUAL, expect: VAR.userId1 }
-  //       }
+  wssCheck:
+  {
+    expectWSActor: [
+      {
+        type: { operator: Operator.EQUAL, expect: API_EVENT.halome.v3.chat.CHANNEL_CREATED },
+        data: {
+          channel: {
+            workspaceId: { operator: Operator.EQUAL, expect: '0' },
+            channelId: { operator: Operator.EQUAL, expect: VAR.channelId },
+            userId: { operator: Operator.EQUAL, expect: VAR.userId },
+          }
+        },
+        includes: {
+          users: [
+            {
+              field: 'userId',
+              operator: Operator.INCLUDE,
+              element: Element.FIRST,
+              expect: [VAR.userId],
+            },
+            {
+              field: 'userType',
+              operator: Operator.INCLUDE,
+              element: Element.FIRST,
+              expect: 0,
+            },
+            {
+              field: 'profile.userBadgeType',
+              operator: Operator.EQUAL,
+              expect: 0,
+            },
+          ],
+        }
+      },
+      {
+        type: { operator: Operator.EQUAL, expect: API_EVENT.halome.v3.chat.MEMBER_JOINED },
+        data: {
+          workspaceId: { operator: Operator.EQUAL, expect: '0' },
+          channelId: { operator: Operator.EQUAL, expect: VAR.channelId },
+          joinedUserId: { operator: Operator.EQUAL, expect: VAR.userId1 }
+        }
+      },
+      {
+        type: { operator: Operator.EQUAL, expect: API_EVENT.halome.v3.chat.MESSAGE_CREATED },
+        data: {
+          message: {
+            channelId: { operator: Operator.EQUAL, expect: VAR.channelId },
+            content: { operator: Operator.EQUAL, expect: '%s joined this channel' },
+            messageType: { operator: Operator.EQUAL, expect: 1 }
+          }
+        }
+      },
+      {
+        type: { operator: Operator.EQUAL, expect: API_EVENT.halome.v3.chat.MESSAGE_CREATED },
+        data: {
+          message: {
+            workspaceId: { operator: Operator.EQUAL, expect: '0' },
+            channelId: { operator: Operator.EQUAL, expect: VAR.channelId },
+            content: { operator: Operator.EQUAL, expect: 'user send message' },
+            userId: { operator: Operator.EQUAL, expect: VAR.userId },
+            messageType: { operator: Operator.EQUAL, expect: 0 }
+          }
+        }
+      },
+      {
+        type: { operator: Operator.EQUAL, expect: API_EVENT.halome.v3.chat.USER_UNREAD_MESSAGE_UPDATED },
+        data: {
+          message: {
+            lastSeenMessageId: { operator: Operator.EQUAL, expect: VAR.messageId3 },
+            channelId: { operator: Operator.EQUAL, expect: VAR.channelId },
+          }
+        },
+      }
+    ],
+    expectWSRecipient: [
+      {
+        type: { operator: Operator.EQUAL, expect: API_EVENT.halome.v3.chat.MEMBER_JOINED },
+        data: {
+          workspaceId: { operator: Operator.EQUAL, expect: '0' },
+          channelId: { operator: Operator.EQUAL, expect: VAR.channelId },
+          joinedUserId: { operator: Operator.EQUAL, expect: VAR.userId1 }
+        }
 
-  //     },
-  //     {
-  //       type: { operator: Operator.EQUAL, expect: API_EVENT.halome.v3.chat.MESSAGE_CREATED },
-  //       data: {
-  //         message: {
-  //           channelId: { operator: Operator.EQUAL, expect: VAR.channelId },
-  //           content: { operator: Operator.EQUAL, expect: '%s joined this channel' },
-  //           messageType: { operator: Operator.EQUAL, expect: 1 }
-  //         }
-  //       }
+      },
+      {
+        type: { operator: Operator.EQUAL, expect: API_EVENT.halome.v3.chat.MESSAGE_CREATED },
+        data: {
+          message: {
+            channelId: { operator: Operator.EQUAL, expect: VAR.channelId },
+            content: { operator: Operator.EQUAL, expect: '%s joined this channel' },
+            messageType: { operator: Operator.EQUAL, expect: 1 }
+          }
+        }
 
-  //     },
-  //     {
-  //       type: { operator: Operator.EQUAL, expect: API_EVENT.halome.v3.chat.MESSAGE_CREATED },
-  //       data: {
-  //         message: {
-  //           workspaceId: { operator: Operator.EQUAL, expect: '0' },
-  //           channelId: { operator: Operator.EQUAL, expect: VAR.channelId },
-  //           content: { operator: Operator.EQUAL, expect: 'user send message' },
-  //           userId: { operator: Operator.EQUAL, expect: VAR.userId },
-  //           messageType: { operator: Operator.EQUAL, expect: 0 }
-  //         }
-  //       },
+      },
+      {
+        type: { operator: Operator.EQUAL, expect: API_EVENT.halome.v3.chat.MESSAGE_CREATED },
+        data: {
+          message: {
+            workspaceId: { operator: Operator.EQUAL, expect: '0' },
+            channelId: { operator: Operator.EQUAL, expect: VAR.channelId },
+            content: { operator: Operator.EQUAL, expect: 'user send message' },
+            userId: { operator: Operator.EQUAL, expect: VAR.userId },
+            messageType: { operator: Operator.EQUAL, expect: 0 }
+          }
+        },
 
-  //     },
-  //   ]
-  // }
+      },
+    ]
+  }
 };
