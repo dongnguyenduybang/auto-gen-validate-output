f3e39b6ee1d05c81ba1cc951ed938fc21d89329d
dongnguyenduybang <dndb20011512@gmail.com>
2025-04-23 17:31:00 +0700

    feat: example ws .spec

--- src/auto-gen/test-ws/send-message/send-message.ws.spec.ts
+++ src/auto-gen/test-ws/send-message/send-message.ws.spec.ts
@@ -18,31 +18,19 @@ describe('Test sagas for send-message', () => {
     globalContext = new TestContext();
     globalWSSContext = new WSSContext();
 
-    console.log('beforeAll config:', SendMessageSagaWS.beforeAll);
     const resultBeforeAll = await executeAllSteps(SendMessageSagaWS.beforeAll, globalContext, globalWSSContext);
-    console.log('beforeAll result:', resultBeforeAll);
-    globalContext.debug();
-    globalWSSContext.debug();
 
-    console.log('wsOpen config:', SendMessageSagaWS.wsOpen);
     const resultOpenWS = await executeOpenWS(SendMessageSagaWS.wsOpen, globalContext, globalWSSContext);
-    console.log('wsOpen result:', resultOpenWS);
-    globalContext.debug();
-    globalWSSContext.debug();
-  }, 60000); // Tăng timeout lên 60s để đảm bảo đủ thời gian cho beforeAll
+
+  }, 10000); // Tăng timeout lên 60s để đảm bảo đủ thời gian cho beforeAll
 
   it('should validate response structure', async () => {
     testNumber++;
 
-
-    console.log('steps config:', SendMessageSagaWS.steps);
     const resultSteps = await executeWSSteps(SendMessageSagaWS.steps, globalContext, globalWSSContext);
-    console.log('steps result:', resultSteps);
-    globalContext.debug();
-    globalWSSContext.debug();
 
     expect(resultSteps.every(step => step.status)).toBe(true);
-  }, 240000); // Giữ timeout 240s vì có nhiều bước
+  }, 15000); // Giữ timeout 240s vì có nhiều bước
 
   // Không cần afterAll vì WebSocket đã được đóng trong ws-execute-ws-step.ts và ws-open.ts
 });
