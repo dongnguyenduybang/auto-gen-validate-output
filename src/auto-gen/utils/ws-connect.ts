cb00fc45fd4b2a0f3729fe36fd20dee3e0e9001f
dongnguyenduybang <dndb20011512@gmail.com>
2025-04-23 17:27:14 +0700

    delete: delete unused

--- src/auto-gen/utils/ws-connect.ts
+++
@@ -1,8 +0,0 @@
-import WebSocket from 'ws'
-import { initSocketEvents } from '../functions/connect-ws';
-
-export async function connectWSS(path: string, maxEvents,timeoutMs ): Promise<{ ws: WebSocket, message: any }> {
-    const ws = new WebSocket(path);
-    const message = await initSocketEvents(ws, maxEvents, timeoutMs );
-    return { ws, message };
-}
