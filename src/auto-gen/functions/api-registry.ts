8353599a0d2dedfb868e09ea2ea4f410576b97d4
Bang Dong <54447688+dongnguyenduybang@users.noreply.github.com>
2025-05-05 10:38:04 +0700

    fix: remove accept message requests

--- src/auto-gen/functions/api-registry.ts
+++ src/auto-gen/functions/api-registry.ts
@@ -4,17 +4,6 @@ import { ApiConfig } from './types';
 type ApiRegistry = Record<string, ApiConfig>;
 
 const API_REGISTRY: ApiRegistry = {
-  acceptMessageRequest: {
-    defaultPath: '/Channel/AcceptMessageRequest',
-    requiredHeaders: {
-      'x-session-token': {
-        //headerName
-        source: 'token', //value
-        errorMessage: 'Token not found to accept message', //error
-      },
-    },
-    payloadMapper: (body) => ({ userId: body.userId }),
-  },
   mockUser: {
     defaultPath: '/InternalFaker/MockUsers',
     defaultMethod: 'post',
