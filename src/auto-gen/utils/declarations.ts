874b909ad5f20beb43bc018602058c8e4aaab242
dongnguyenduybang <dndb20011512@gmail.com>
2025-05-07 09:01:31 +0700

    feat: add declarations

---
+++ src/auto-gen/utils/declarations.ts
@@ -0,0 +1,76 @@
+import { TestContext } from "./text-context";
+
+export interface ValidationError {
+  path: string;
+  expected: string;
+  actual: any;
+  message?: string;
+}
+export interface Step {
+  action: string;
+  method?: string;
+  path?: string;
+  body?: any;
+  headers?: any;
+  expect?: any;
+}
+
+export interface StepResult {
+  type?: string;
+  status: boolean;
+  stepName: string;
+  error?: string;
+}
+
+export interface IContext {
+  getValue(path: string | string[]): any;
+  setValue(key: string, value: any): void;
+  mergeData(newData: Record<string, any>): void;
+  debug(): void;
+}
+
+export type ExtractConfig = {
+  [key: string]: {
+    path: string[];
+    fields: string[];
+  };
+};
+
+export interface ApiStep {
+  action: string;
+  method?: string;
+  path?: string;
+  body?: any;
+  header?: any;
+  expect?: any;
+}
+
+export interface ApiConfig {
+  defaultPath: string;
+  defaultMethod?: string;
+  requiredHeaders?: {
+    [headerName: string]: {
+      source: string;
+      errorMessage: string;
+    };
+  };
+  payloadMapper?: (body: any) => any;
+  context?: TestContext;
+}
+
+export interface ApiResponse {
+  ok: boolean;
+  data?: any;
+  error?: any;
+}
+
+export interface ApiFunctionParams {
+  method?: string;
+  path?: string;
+  headers: any;
+  body: any;
+}
+
+
+export type ActionHandler = (dtoName: string) => Promise<void> | void;
+export type ApiRegistry = Record<string, ApiConfig>;
