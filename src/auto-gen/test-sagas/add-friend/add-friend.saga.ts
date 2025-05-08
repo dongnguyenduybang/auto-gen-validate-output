9bc298054d4e4a13efb99a6c9a75a0fd1339d05d
dongnguyenduybang <dndb20011512@gmail.com>
2025-05-08 13:34:29 +0700

    fix: fix saga new config

--- src/auto-gen/test-sagas/add-friend/add-friend.saga.ts
+++ src/auto-gen/test-sagas/add-friend/add-friend.saga.ts
@@ -1,35 +1,12 @@
 import { SagaTestSuite } from '../../utils/declarations';
 import {
-  HeaderList,
   Operator,
   VAR,
   ACTION,
+  HEADER_LIST,
 } from '../../enums';
 
 export const AddFriendSaga: SagaTestSuite = {
-  beforeAll: [
-    {
-      title: 'Mock User',
-      step: {
-        action: ACTION.MOCK_USER,
-        body: {
-          quantity: 2,
-          prefix: 'testabcssd',
-          badge: 0,
-        },
-      },
-    },
-    {
-      title: 'Create Channel',
-      step: {
-        action: ACTION.CREATE_CHANNEL,
-        body: {
-          name: 'channel name',
-          workspaceId: VAR.workspaceId,
-        },
-      },
-    },
-  ],
   steps: [
     {
       title: 'Add Friend Saga',
@@ -39,7 +16,9 @@ export const AddFriendSaga: SagaTestSuite = {
           body: {
             userId: VAR.userId1,
           },
-          headers: HeaderList.Token(),
+          headers: HEADER_LIST.create({
+            token: VAR.token,
+          }),
           expect: {
             ok: { operator: Operator.EQUAL, expect: true },
           },
@@ -47,7 +26,7 @@ export const AddFriendSaga: SagaTestSuite = {
       ],
     },
     {
-      title: 'Add Friend Saga 1',
+      title: 'Send Message After Add Friend',
       step: [
         {
           action: ACTION.SEND_DM_MESSAGE,
@@ -56,38 +35,14 @@ export const AddFriendSaga: SagaTestSuite = {
             content: 'Hello',
             ref: 'ref',
           },
-          headers: HeaderList.Token(),
-          expect: {
-            ok: { operator: Operator.EQUAL, expect: false },
-          },
-        },
-        {
-          action: ACTION.SEND_DM_MESSAGE,
-          body: {
-            userId: VAR.userId1,
-            content: 'Hello1',
-            ref: 'ref',
-          },
-          headers: HeaderList.Token(),
+          headers: HEADER_LIST.create({
+            token: VAR.token,
+          }),
           expect: {
             ok: { operator: Operator.EQUAL, expect: false },
           },
         },
       ],
-    },
-  ],
-  afterAll: [
-    {
-      title: 'Delete User',
-      step: {
-        action: ACTION.DELETE_MOCKED_USER,
-        body: {
-          prefix: 'testabcssd',
-        },
-        expect: {
-          ok: { operator: Operator.EQUAL, expect: false },
-        },
-      },
     },
   ],
 };
