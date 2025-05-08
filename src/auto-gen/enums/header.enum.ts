410b719fcfdd6e56db19a3cde5c8d0fb4d10c1ef
dongnguyenduybang <dndb20011512@gmail.com>
2025-05-08 13:34:09 +0700

    fix: fix header list & add more header item

--- src/auto-gen/enums/header.enum.ts
+++ src/auto-gen/enums/header.enum.ts
@@ -1,8 +1,22 @@
-export const HeaderList = {
-  Token: (token = '{{token}}') => ({
-    'x-session-token': token,
-  }),
-  Token1: () => HeaderList.Token('{{token1}}'),
-  Token2: () => HeaderList.Token('{{token2}}'),
-  Token3: () => HeaderList.Token('{{token3}}'),
+import { HeaderOptions } from "../utils/declarations";
+
+export const HEADER_LIST = {
+  create: ({
+    token,
+    userId,
+    deviceId,
+    role,
+    ...customHeaders
+  }: HeaderOptions) => {
+    const headers: Record<string, string> = {
+      'x-session-token': token,
+      'x-user-id': userId,
+      ...customHeaders,
+    };
+
+    if (deviceId) headers['x-device-id'] = deviceId;
+    if (role) headers['x-role'] = role;
+
+    return headers;
+  },
 };
