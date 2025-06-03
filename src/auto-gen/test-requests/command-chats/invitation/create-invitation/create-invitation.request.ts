f3fa89cf450c912e4ababc94d35360e4a35378ba
dongnguyenduybang <dndb20011512@gmail.com>
2025-06-03 16:58:05 +0700

    feat: create-invitation

---
+++ src/auto-gen/test-requests/command-chats/invitation/create-invitation/create-invitation.request.ts
@@ -0,0 +1,21 @@
+import { ACTION, HEADER_LIST, VAR } from "../../enums";
+import { RequestTestSuite } from "../../utils/declarations";
+
+export const CreateInvitationRequest: RequestTestSuite = {
+  action: ACTION.CREATE_CHANNEL,
+  headers: HEADER_LIST.create({ token: VAR.token }),
+  body: {
+    workspaceId: VAR.workspaceId,
+    channelId: VAR.channelId,
+    expiresIn: 1000,
+    maxUsers: 1
+  },
+  options: [
+    {   
+      beforeAll: [],
+      beforeEach: [],
+      afterEach: [],
+      afterAll: []
+    },
+  ],
+};
