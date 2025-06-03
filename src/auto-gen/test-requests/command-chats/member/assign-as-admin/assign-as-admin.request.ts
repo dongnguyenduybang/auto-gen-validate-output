159f50e8e7a0262eb28ec1b3b36fd5979fa659cb
dongnguyenduybang <dndb20011512@gmail.com>
2025-06-03 16:59:40 +0700

    feat: assign-as-admin

---
+++ src/auto-gen/test-requests/command-chats/member/assign-as-admin/assign-as-admin.request.ts
@@ -0,0 +1,25 @@
+import { VAR, ACTION, HEADER_LIST } from '../../../../enums';
+
+export const AssignAsAdminRequest = {
+  action: ACTION.ASSIGN_AS_ADMIN,
+  body: {
+    channelId: VAR.channelId,
+    workspaceId: VAR.workspaceId,
+    userId: VAR.userId1,
+  },
+  headers: HEADER_LIST.create({ token: VAR.token }),
+  options: [
+    {
+      beforeAll: [{
+        action: ACTION.ACCEPT_INVITATION,
+        headers: HEADER_LIST.create({token: VAR.token1}),
+        body: {
+            invitationLink: VAR.invitationLink
+        }
+      }],
+      beforeEach: [],
+      afterEach: [],
+      afterAll: []
+    },
+  ],
+};
