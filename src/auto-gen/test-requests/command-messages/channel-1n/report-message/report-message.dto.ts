1d4c38b527378bad76ea00a11719e08111db146a
dongnguyenduybang <dndb20011512@gmail.com>
2025-06-03 17:04:57 +0700

    feat: report-message

---
+++ src/auto-gen/test-requests/command-messages/channel-1n/report-message/report-message.dto.ts
@@ -0,0 +1,40 @@
+import { ErrorMessage, PretendingTo, ReportCategory } from "../../../../enums";
+import { IsDefined, IsEnum, IsInvalid, IsNotEmpty, IsString, ValidIf, MinLength, MaxLength, IsEmoji, GenEmoji, IsULID } from "../../../../decorator";
+
+export class ReportMessageDTO {
+    @IsDefined({ message: ErrorMessage.COULD_NOT_PERMISSION })
+    @IsInvalid({ message: ErrorMessage.INVALID_CHANNEL })
+    @IsNotEmpty({ message: ErrorMessage.COULD_NOT_PERMISSION })
+    @IsString({ message: ErrorMessage.COULD_NOT_PERMISSION })
+    workspaceId: string = '';
+
+    @IsDefined({ message: `Could not resolve permission type` })
+    @IsInvalid({ message: ErrorMessage.INVALID_CHANNEL })
+    @IsNotEmpty({ message: `Could not resolve permission type` })
+    @IsString({ message: `Could not resolve permission type` })
+    channelId: string = '';
+
+    @IsString()
+    @IsNotEmpty()
+    @IsULID()
+    @IsDefined()
+    messageId: string = '';
+
+    @IsEnum(ReportCategory)
+    @IsDefined()
+    @IsNotEmpty()
+    reportCategory: ReportCategory = 0
+
+    @ValidIf('reportCategory', '===', ReportCategory.REPORT_CATEGORY_OTHER, { optional: false })
+    @IsDefined()
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(1)
+    @MaxLength(255)
+    reportReason: string = ''
+
+    @ValidIf('reportCategory', '===', ReportCategory.REPORT_CATEGORY_PRETENDING_TO_BE_SOMEONE, { optional: false })
+    @IsEnum(PretendingTo)
+    @IsDefined()
+    pretendingTo: PretendingTo = 0
+}
---
+++ src/auto-gen/test-requests/command-messages/channel-1n/report-message/report-message.request.ts
@@ -0,0 +1,41 @@
+import { ACTION, HEADER_LIST, PretendingTo, ReportCategory, VAR } from "../../../../enums";
+import { RequestTestSuite } from "../../../../utils/declarations";
+
+export const ReportMessageRequest: RequestTestSuite = {
+  action: ACTION.REPORT_MESSAGE,
+  headers: HEADER_LIST.create({ token: VAR.token }),
+  body: {
+    channelId: VAR.channelId,
+    workspaceId: VAR.workspaceId,
+    messageId: VAR.messageId1,
+    reportCategory: ReportCategory.REPORT_CATEGORY_PRETENDING_TO_BE_SOMEONE,
+    pretendingTo: PretendingTo.PRETENDING_TO_FRIEND,
+    reportReason: 'report message',
+  },
+  options: [
+    {
+      beforeAll: [
+        {
+          action: ACTION.ACCEPT_INVITATION,
+          headers: HEADER_LIST.create({token: VAR.token1}),
+          body: {
+            invitationLink: VAR.invitationLink
+          }
+        },
+                {
+          action: ACTION.SEND_MESSAGE,
+          headers: HEADER_LIST.create({token: VAR.token}),
+          body: {
+            workspaceId: VAR.workspaceId,
+            content: 'aaaaa',
+            channelId: VAR.channelId,
+            ref: 'ref'
+          }
+        }
+      ],
+      beforeEach: [],
+      afterEach: [],
+      afterAll: []
+    },
+  ],
+};
