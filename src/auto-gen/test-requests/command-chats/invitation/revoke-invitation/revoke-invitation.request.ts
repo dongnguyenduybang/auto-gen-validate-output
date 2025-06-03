8f74b3fad24ec388330eaebb5759ee114a0fdd90
dongnguyenduybang <dndb20011512@gmail.com>
2025-06-03 16:58:35 +0700

    feat: revoke-invitation

---
+++ src/auto-gen/test-requests/command-chats/invitation/revoke-invitation/revoke-invitation.request.ts
@@ -0,0 +1,28 @@
+import { VAR, ACTION, HEADER_LIST } from '../../../../enums';
+
+export const RevokeInvitationRequest = {
+  action: ACTION.REVOKE_INVITATION,
+  body: {
+    channelId: VAR.channelId,
+    workspaceId: VAR.workspaceId,
+    code: VAR.code,
+  },
+  headers: HEADER_LIST.create({ token: VAR.token }),
+  options: [
+    {
+      beforeAll: [{
+        action: ACTION.CREATE_INVITATION,
+        headers: HEADER_LIST.create({ token: VAR.token }),
+        body: {
+          workspaceId: VAR.workspaceId,
+          channelId: VAR.channelId,
+          expiresIn: 10000,
+          maxUses: 1
+        }
+      }],
+      beforeEach: [],
+      afterEach: [],
+      afterAll: []
+    },
+  ],
+};
