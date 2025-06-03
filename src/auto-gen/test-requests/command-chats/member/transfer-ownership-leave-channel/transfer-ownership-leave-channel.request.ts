8871fa2b39536feee532ca73bceeb2894587db95
dongnguyenduybang <dndb20011512@gmail.com>
2025-06-03 17:02:57 +0700

    feat: transfer-ownership-leave-channel

---
+++ src/auto-gen/test-requests/command-chats/member/transfer-ownership-leave-channel/transfer-ownership-leave-channel.request.ts
@@ -0,0 +1,28 @@
+import { VAR, ACTION, HEADER_LIST } from '../../../../enums';
+
+export const TransferOwnershipLeaveChannelRequest = {
+  action: ACTION.TRANSFER_OWNERSHIP_LEAVE_CHANNEL,
+  body: {
+    channelId: VAR.channelId,
+    workspaceId: VAR.workspaceId,
+    userId: VAR.userId1,
+  },
+  headers: HEADER_LIST.create({ token: VAR.token }),
+  options: [
+    {
+      beforeAll: [
+        {
+          action: ACTION.ACCEPT_INVITATION,
+          headers: HEADER_LIST.create({ token: VAR.token1 }),
+          body: {
+            invitationLink: VAR.invitationLink
+          }
+        },
+        
+      ],
+      beforeEach: [],
+      afterEach: [],
+      afterAll: []
+    },
+  ],
+};
