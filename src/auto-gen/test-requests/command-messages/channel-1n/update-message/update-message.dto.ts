22e093eba3044bf63420e91a54d3789b64f35345
dongnguyenduybang <dndb20011512@gmail.com>
2025-06-03 17:06:54 +0700

    feat: update-message

---
+++ src/auto-gen/test-requests/command-messages/channel-1n/update-message/update-message.dto.ts
@@ -0,0 +1,41 @@
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsULID,
+  IsInvalid,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from '../../../../decorator';
+
+export class UpdateMessageDTO {
+  @IsDefined({ message: `Could not resolve permission type` })
+  @IsInvalid({ message: `Invalid channel` })
+  @IsNotEmpty({ message: `Could not resolve permission type` })
+  @IsString({ message: `Could not resolve permission type` })
+  workspaceId: string = '';
+
+  @IsDefined({ message: `Could not resolve permission type` })
+  @IsInvalid({ message: `Unsupported permission type` })
+  @IsNotEmpty({ message: `Could not resolve permission type` })
+  @IsString({ message: `Could not resolve permission type` })
+  channelId: string = '';
+
+  @IsString()
+  @IsULID()
+  @IsDefined()
+  messageId: string = '';
+
+  @IsString()
+  @IsDefined()
+  @MinLength(1)
+  @MaxLength(2000)
+  content: string = '';
+
+  @IsOptional()
+  @IsString()
+  @IsDefined()
+  @MinLength(1)
+  ref: string = '';
+}
---
+++ src/auto-gen/test-requests/command-messages/channel-1n/update-message/update-message.request.ts
@@ -0,0 +1,40 @@
+import { VAR, ACTION, HEADER_LIST } from '../../../../enums';
+
+export const UpdateMessageRequest = {
+  action: ACTION.UPDATE_MESSAGE,
+  body: {
+    channelId: VAR.channelId,
+    messageId: VAR.messageId1,
+    workspaceId: '0',
+    content: 'test response update message',
+    ref: 'ref',
+  },
+  headers: HEADER_LIST.create({ token: VAR.token }),
+  options: [
+    {
+      beforeAll: [],
+      beforeEach: [
+        {
+          action: ACTION.SEND_MESSAGE,
+          body: {
+            workspaceId: '0',
+            channelId: VAR.channelId,
+            content: 'duybang12345',
+            ref: 'abc',
+          },
+          headers: HEADER_LIST.create({ token: VAR.token }),
+        },
+      ],
+      afterEach: [],
+      afterAll: [
+        {
+          action: ACTION.DELETE_MOCKED_USER,
+          body: {
+            prefix: 'testUpdateMessage',
+          },
+        },
+      ]
+    },
+
+  ],
+};
