a6b6082641b864838a4196a3cc776db5fe69765e
dongnguyenduybang <dndb20011512@gmail.com>
2025-06-03 17:03:35 +0700

    feat: update-nickname

---
+++ src/auto-gen/test-requests/command-chats/member/update-nickname/update-nickname.dto.ts
@@ -0,0 +1,36 @@
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsInvalid,
+  IsString,
+  MaxLength,
+  MinLength,
+  IsNotNull,
+} from '../../../../decorator';
+
+export class UpdateNicknameDTO {
+  @IsDefined({ message: `Could not resolve permission type` })
+  @IsInvalid({ message: `Invalid channel` })
+  @IsNotEmpty({ message: `Could not resolve permission type` })
+  @IsString({ message: `Could not resolve permission type` })
+  workspaceId: string = '';
+
+  @IsDefined({ message: `Could not resolve permission type` })
+  @IsInvalid({ message: `Unsupported permission type` })
+  @IsNotEmpty({ message: `Could not resolve permission type` })
+  @IsString({ message: `Could not resolve permission type` })
+  channelId: string = '';
+
+  @IsString({ message: `Could not resolve permission type` })
+  @IsDefined({ message: `Could not resolve permission type` })
+  @IsNotEmpty({ message: `Could not resolve permission type` })
+  @IsInvalid({ message: `Unauthorized request` })
+  @IsNotNull({ message: `Could not resolve permission type`})
+  userId: string = '';
+
+  @IsString()
+  @IsDefined()
+  @MinLength(1)
+  @MaxLength(50)
+  nickname: string = '';
+}
