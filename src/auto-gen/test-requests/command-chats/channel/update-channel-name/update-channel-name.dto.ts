34d7ed7f5656fda90242760f57939b501dcdf9da
dongnguyenduybang <dndb20011512@gmail.com>
2025-06-03 16:57:12 +0700

    feat: update-channel-name

---
+++ src/auto-gen/test-requests/command-chats/channel/update-channel-name/update-channel-name.dto.ts
@@ -0,0 +1,29 @@
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsInvalid,
+  IsString,
+  MaxLength,
+  MinLength,
+} from '../../../../decorator';
+
+export class UpdateChannelNameDTO {
+  @IsDefined({ message: `Could not resolve permission type` })
+  @IsInvalid({ message: `Invalid channel` })
+  @IsNotEmpty({ message: `Could not resolve permission type` })
+  @IsString({ message: `Could not resolve permission type` })
+  workspaceId: string = '';
+
+  @IsDefined({ message: `Could not resolve permission type` })
+  @IsInvalid({ message: `Invalid channel` })
+  @IsNotEmpty({ message: `Could not resolve permission type` })
+  @IsString({ message: `Could not resolve permission type` })
+  channelId: string = '';
+
+  @IsString()
+  @IsDefined()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
+  name: string = ''
+}
