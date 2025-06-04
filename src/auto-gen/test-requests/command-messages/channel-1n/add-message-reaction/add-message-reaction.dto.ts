dca6780f7e2278a4d1a2f03c531f60324d457ef5
dongnguyenduybang <dndb20011512@gmail.com>
2025-06-04 17:19:20 +0700

    fix: fix add message reaction

--- src/auto-gen/test-requests/command-messages/channel-1n/add-message-reaction/add-message-reaction.dto.ts
+++ src/auto-gen/test-requests/command-messages/channel-1n/add-message-reaction/add-message-reaction.dto.ts
@@ -1,3 +1,4 @@
+import { ErrorMessage } from '../../../../enums';
 import {
   IsDefined,
   IsNotEmpty,
@@ -6,22 +7,27 @@ import {
   IsOptional,
   IsString,
   IsEmoji,
+  IsNotNull,
 } from '../../../../decorator';
 
 export class AddMessageReactionDTO {
-  @IsDefined({ message: `Could not resolve permission type` })
-  @IsInvalid({ message: `Invalid channel` })
-  @IsNotEmpty({ message: `Could not resolve permission type` })
-  @IsString({ message: `Could not resolve permission type` })
+  @IsDefined({ message: ErrorMessage.COULD_NOT_PERMISSION })
+  @IsInvalid({ message: ErrorMessage.INVALID_CHANNEL })
+  @IsNotNull({ message: ErrorMessage.COULD_NOT_PERMISSION })
+  @IsNotEmpty({ message: ErrorMessage.COULD_NOT_PERMISSION })
+  @IsString({ message: ErrorMessage.COULD_NOT_PERMISSION })
   workspaceId: string = '';
 
-  @IsDefined({ message: `Could not resolve permission type` })
-  @IsInvalid({ message: `Invalid channel` })
-  @IsNotEmpty({ message: `Could not resolve permission type` })
-  @IsString({ message: `Could not resolve permission type` })
+  @IsDefined({ message: ErrorMessage.UNSUPPORTED_PERMISSION_TYPE })
+  @IsInvalid({ message: ErrorMessage.INVALID_CHANNEL })
+  @IsNotNull({ message: ErrorMessage.COULD_NOT_PERMISSION })
+  @IsNotEmpty({ message: ErrorMessage.COULD_NOT_PERMISSION })
+  @IsString({ message: ErrorMessage.COULD_NOT_PERMISSION })
   channelId: string = '';
 
   @IsString()
+  @IsNotEmpty()
+  @IsNotNull()
   @IsULID()
   @IsDefined()
   messageId: string = '';
@@ -30,6 +36,8 @@ export class AddMessageReactionDTO {
   @IsDefined()
   @IsString()
   @IsOptional()
+  @IsNotEmpty()
+  @IsNotNull()
   emoji: string = '';
 
 }
