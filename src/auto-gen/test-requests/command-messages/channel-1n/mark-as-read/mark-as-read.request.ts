03cf88e5bc7df3547309b1ed096c140a2c1bb12b
dongnguyenduybang <dndb20011512@gmail.com>
2025-06-04 17:19:35 +0700

    feat: mark-as-read

---
+++ src/auto-gen/test-requests/command-messages/channel-1n/mark-as-read/mark-as-read.request.ts
@@ -0,0 +1,33 @@
+import { RequestTestSuite } from '../../../../utils/declarations';
+import { ACTION, HEADER_LIST, VAR } from '../../../../enums';
+
+export const MarkAsReadRequest: RequestTestSuite = {
+  action: ACTION.MARK_AS_READ,
+  headers: HEADER_LIST.create({ token: VAR.token }),
+  body: {
+    channelId: VAR.channelId,
+    workspaceId: VAR.workspaceId,
+    messageId: VAR.messageId
+  },
+  options: [
+    {
+      beforeAll: [
+        {
+          action: ACTION.SEND_MESSAGE,
+          body: {
+            workspaceId: VAR.workspaceId,
+            channelId: VAR.channelId,
+            content: 'duybang12345',
+            ref: 'abc',
+          },
+          headers: HEADER_LIST.create({ token: VAR.token }),
+        },
+      ],
+      beforeEach: [
+
+      ],
+      afterEach: [],
+      afterAll: []
+    },
+  ],
+};
