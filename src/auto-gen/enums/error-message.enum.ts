528d5f6f4dfed94c72f3a17792ae049ff014ba79
dongnguyenduybang <dndb20011512@gmail.com>
2025-06-30 13:41:34 +0700

    fix: error message enum

--- src/auto-gen/enums/error-message.enum.ts
+++ src/auto-gen/enums/error-message.enum.ts
@@ -15,7 +15,7 @@ export enum ErrorMessage {
   INVALID_TYPE_ARRAY = 'expected array, received',
   INVALID_TYPE_DATE = 'must be a date',
   INVALID_TYPE_OBJ = 'expected object, received',
-  INVALID_ENUM = 'invalid enum value. Expected',
+  INVALID_ENUM = 'expected',
   INVALID_DATE_OVER_CURRENT = 'must not dates greater than or equal to the current date',
   INVALID_DATE_EQUAL_CURRENT = 'must not dates equal to the current date',
   INVALID_RANGE_NUMBER = 'must be one of the following values:',
