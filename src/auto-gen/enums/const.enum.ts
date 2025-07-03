030d22dab6ac8dc9141f3d3c452e4d8ae8abc7a1
dongnguyenduybang <dndb20011512@gmail.com>
2025-07-03 13:58:10 +0700

    feat: const enum

---
+++ src/auto-gen/enums/const.enum.ts
@@ -0,0 +1,22 @@
+import { ChannelTypeEnum } from "./channel-type.enum";
+
+export enum CONST {
+    workspaceId = '0',
+    code = '{{code}}',
+    avatarPath = 'https://cdn.jsdelivr.net/gh/faker-js/assets-person-portrait/female/512/26.jpg',
+    coverPath = 'https://fs.ugc.ziicdn.net/01JEWASZD3TK58P25Y1QJSYRPC/heic_300kB.heic',
+    latitude = '10.0324312',
+    longitude = '105.7833368',
+    stickerId = '01JPPHQFHA85NG58H2TYTMCXK0',
+    email = 'ziichat@halome.com',
+    ringBackTone = 'https://fs.ugc.ziicdn.net/01JEZJ0GPMSHH2RVX9QTSJJNRX/heic_300kB.heic',
+    ringBackToneId = '{{ringbackToneId}}',
+    defaultEmoji = '🚀',
+    defaultCollectionId = '01JPPHQFHCC697M93K6PTZTZ8Q',
+    name = 'channelname1',
+    versionSwagger = 'V3$Request',
+    content = 'defaultContent',
+    userIds = '["{{VAR.userId1}}"]',
+    channelType = ChannelTypeEnum.CHANNEL_TYPE_ENUM_CHANNEL,
+    ref = 'ref',
+}
