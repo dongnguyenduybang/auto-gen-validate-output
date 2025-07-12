e7d3ada899830eb778361bea699af383236eabea
dongnguyenduybang <dndb20011512@gmail.com>
2025-07-12 15:33:01 +0700

    fix: call API

--- src/auto-gen/enums/const.enum.ts
+++ src/auto-gen/enums/const.enum.ts
@@ -1,22 +1,34 @@
-import { ChannelTypeEnum } from "./channel-type.enum";
+import { ulid } from 'ulidx';
+import { ChannelTypeEnum } from './channel-type.enum';
+import { ExpireAfterTimeEnum } from './expire-after-time.enum';
+import { BadgeEnum } from './badge.enum';
 
-export enum CONST {
-    workspaceId = '0',
-    code = '{{code}}',
-    avatarPath = 'https://cdn.jsdelivr.net/gh/faker-js/assets-person-portrait/female/512/26.jpg',
-    coverPath = 'https://fs.ugc.ziicdn.net/01JEWASZD3TK58P25Y1QJSYRPC/heic_300kB.heic',
-    latitude = '10.0324312',
-    longitude = '105.7833368',
-    stickerId = '01JPPHQFHA85NG58H2TYTMCXK0',
-    email = 'ziichat@halome.com',
-    ringBackTone = 'https://fs.ugc.ziicdn.net/01JEZJ0GPMSHH2RVX9QTSJJNRX/heic_300kB.heic',
-    ringBackToneId = '{{ringbackToneId}}',
-    defaultEmoji = '🚀',
-    defaultCollectionId = '01JPPHQFHCC697M93K6PTZTZ8Q',
-    name = 'channelname1',
-    versionSwagger = 'V3$Request',
-    content = 'defaultContent',
-    userIds = '["{{VAR.userId1}}"]',
-    channelType = ChannelTypeEnum.CHANNEL_TYPE_ENUM_CHANNEL,
-    ref = 'ref',
-}
+export const CONST = {
+  workspaceId: '0',
+  code: '{{code}}',
+  avatarPath:
+    'https://cdn.jsdelivr.net/gh/faker-js/assets-person-portrait/female/512/26.jpg',
+  coverPath:
+    'https://fs.ugc.ziicdn.net/01JEWASZD3TK58P25Y1QJSYRPC/heic_300kB.heic',
+  latitude: '10.0324312',
+  longitude: '105.7833368',
+  stickerId: '01JPPHQFHA85NG58H2TYTMCXK0',
+  email: 'ziichat@halome.com',
+  ringBackTone:
+    'https://fs.ugc.ziicdn.net/01JEZJ0GPMSHH2RVX9QTSJJNRX/heic_300kB.heic',
+  ringBackToneId: '{{ringbackToneId}}',
+  defaultEmoji: '🚀',
+  defaultCollectionId: '01JPPHQFHCC697M93K6PTZTZ8Q',
+  name: 'channelname1',
+  versionSwagger: 'V3$Request',
+  content: 'defaultContent',
+  // userIds: `["{{VAR.userId1}}"]`,
+  channelType: ChannelTypeEnum.CHANNEL_TYPE_ENUM_CHANNEL,
+  ref: 'ref',
+  prefix: 'defaultPrefix',
+  quantity: 2,
+  badge: BadgeEnum.USER_BADGE_TYPE_DEFAULT,
+  nextPageToken: ulid(), // ✅ OK
+  status: '🚀',
+  expireAfterTime: ExpireAfterTimeEnum.USER_STATUS_EXPIRES_AFTER_TIME_ENUM_AFTER_1_HOUR
+} as const;
